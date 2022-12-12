1407602f802ada704593b279cf73b397379deaba
Alekseiv25 <alekseiv25@gmail.com>
2022-12-12 16:41:07 +0300

    final ver

--- Wildberries/scripts/util.js
+++
@@ -1,4 +0,0 @@
-export const korzina = document.querySelector('.btn-outline-success')
-export const modal = document.querySelector('.modal')
-export const modalList = document.querySelector('.modal-ul')
-export const btnSumma = document.querySelector('.summa')
